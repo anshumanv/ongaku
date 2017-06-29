5b98f95e708a71bb89b2700fcee0cf70aadcff12
Anshuman Verma <anshu.av97@gmail.com>
2017-06-29 17:11:34 +0530

    get basic structure down

--- ongaku.js
+++ ongaku.js
@@ -1,4 +1,27 @@
-window.addEventListener('load', function () {
-	const player = document.querySelector('.player');
-	console.log(player);
-})
+window.addEventListener("load", function () {
+
+	// Grabbing elements from the page
+	const music = document.querySelector('#music');
+	const player = document.querySelector('#player');
+	const playButton = document.querySelector('#pButton');
+	const playHead = document.querySelector('#playhead');
+
+
+	// Section for functions
+	function togglePlay () {	// function to toggle play/pause
+		if (music.paused) {
+			music.play();
+		} else 
+			music.pause();
+
+		music.classList.toggle('play');
+	}
+
+
+	console.log(music.attributes)
+
+	// Section for handling events
+	playButton.addEventListener('click', togglePlay);
+
+	
+});
