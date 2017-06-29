f4ae62389ff4f9812bc1fd58cf635e16463d0e10
Anshuman Verma <anshu.av97@gmail.com>
2017-06-29 18:24:30 +0530

    progress bar made functional

--- ongaku.js
+++ ongaku.js
@@ -9,19 +9,31 @@ window.addEventListener("load", function () {
 
 	// Section for functions
 	function togglePlay () {	// function to toggle play/pause
-		if (music.paused) {
-			music.play();
-		} else 
-			music.pause();
+		music[music.paused ? 'play' : 'pause']();
+	}
 
-		music.classList.toggle('play');
+	function updateButton () {
+		if(!music.paused){
+			playButton.classList.remove('play');
+			playButton.classList.add('pause');
+		}
+		else {
+			playButton.classList.remove('pause');
+			playButton.classList.add('play');
+		}
+	}
+	
+	function handleProgress () {
+		const percent = (music.currentTime / music.duration ) * 100;
+		playHead.style.marginLeft = (percent / 100) * 364  + "px";
 	}
 
 
-	console.log(music.attributes)
-
 	// Section for handling events
 	playButton.addEventListener('click', togglePlay);
+	playButton.addEventListener('click', updateButton);
 
+	music.addEventListener('timeupdate', handleProgress);
 	
+
 });
