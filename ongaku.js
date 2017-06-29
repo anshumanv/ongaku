10ed912fa217873f412fb2d750d54cc1b71baa4b
Anshuman Verma <anshu.av97@gmail.com>
2017-06-29 20:50:52 +0530

    rm poor spread

--- ongaku.js
+++ ongaku.js
@@ -45,9 +45,9 @@ window.addEventListener("load", function () {
 	// The de-facto unbiased shuffle algorithm is the Fisher-Yates (aka Knuth) Shuffle (based on SE answer).
 	function shuffle(array) {
 		var currentIndex = array.length, temporaryValue, randomIndex;
-		// While there remain elements to shuffle...
+		// While there remain elements to shuffle
 		while (0 !== currentIndex) {
-		// Pick a remaining element...
+			// Pick a remaining element
 			randomIndex = Math.floor(Math.random() * currentIndex);
 			currentIndex -= 1;
 			// And swap it with the current element.
