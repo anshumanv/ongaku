2f7080d09cb128f202abdee05aa25d8e93e5a576
Anshuman Verma <anshu.av97@gmail.com>
2017-07-01 00:00:50 +0530

    Add danganronpa op

--- ongaku.js
+++ ongaku.js
@@ -123,6 +123,12 @@ data = [
 				img : "https://pbs.twimg.com/media/C-FAE86UQAAdckd.jpg"
 			},
 
+			{
+				link : "http://lyricmp3skull.org/s363640c/file/danganronpa-anime-op-tv-size/249496430.mp3",
+				name : Danganronpa The Animation OP,
+				img : "http://www.imgbase.info/images/safe-wallpapers/anime/dangan_ronpa/37988_dangan_ronpa.jpg"
+			},
+
 
 			// Store endings here for now
 			{
