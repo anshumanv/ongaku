eb9007a88253c7bd21c9f6b0755a3aabc20ccc07
Anshuman Verma <anshu.av97@gmail.com>
2017-07-02 23:54:51 +0530

    update bnha song link

--- js/ongaku.js
+++ js/ongaku.js
@@ -112,7 +112,7 @@ data = [
 			},
 
 			{
-				link : "http://lyricmp3skull.org/s363640c/file/boku-no-hero-academia-op-1/261606779.mp3",
+				link : "http://w33.youtubeinmp3.com/download/get/?id=5N3BViqaqZo&r=q5pSUDUqeC1tH2IDq6K0VnHiGYRs0uas&t=Boku+no+Hero+Academia+Season+2+opening+1+Full%E3%80%8EKenshi+Yonezu-+Peace+Sign%E3%80%8F&progressType=button",
 				name : "Boku no Hero Academia S1 OP-1",
 				img : "http://www.ricedigital.co.uk/wp-content/uploads/2017/03/003-1.jpg"
 			},
