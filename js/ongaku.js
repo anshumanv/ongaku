65a0e914c14128d060f85ee1a11ce8c98fc4e313
Anshuman Verma <anshu.av97@gmail.com>
2017-07-03 20:46:46 +0530

    update light's theme

--- js/ongaku.js
+++ js/ongaku.js
@@ -163,7 +163,7 @@ data = [
 
 			//  Store OSTs here for now
 			{
-				link : "http://lyricmp3skull.org/s363640c/file/death-note-ls-theme/18366160.mp3",
+				link : "http://66.90.93.122/ost/death-note-original-soundtrack/fghgpcdtbm/04-l-s-theme.mp3",
 				name : "L's Theme",
 				img : "http://www.imgbase.info/images/safe-wallpapers/anime/death_note/3579-anime_death_note_wallpaper.jpg"
 			},
