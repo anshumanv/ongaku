ecf24560fdb94fd808f84c5bc5652e97beab2504
Aashutosh Rathi <aashutoshrathi@gmail.com>
2017-09-03 00:13:31 +0530

    Added test.sh with Travis CI (#122)
    * Added test.sh with Travis
    
    * Added Links to whitelist
    
    * Allowed Unicode Error
    
    * Error to whitelist
    
    * Compromise done as one link have non-ASCII char
    
    * mp3fb.com added to whitelist

---
+++ test.sh
@@ -0,0 +1,16 @@
+fname=js/data.json
+call="awesome_bot $fname --skip-save-results --allow-redirect -w cs1.mp3.pm,mp3fb.com"
+
+out=$($call)
+if [[ $out != *"No issues"* ]]; then
+	echo $out
+	echo "<<< Failed to connect, retrying.. >>>"
+	out=$($call)
+fi
+
+echo $out
+if [[ $out != *"No issues"* ]]; then
+	exit 1 # Exit with 1 means Build Failed
+else
+	exit 0 # Exit with 0 means Build Passed
+fi
